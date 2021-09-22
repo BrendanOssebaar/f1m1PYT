c3bf49270178d88e576aeca2b9131030ad3eef9b
BrendanOssebaar <34114@ma-web.nl>
2021-09-22 21:49:18 +0200

    My First Operations

---
+++ MyFirstOperations.py
@@ -0,0 +1,10 @@
+a = 24
+b = 7
+print("a = ", + a)
+print("b = ", + b)
+print("a + b =", a+b)
+print("a - b =", a-b)
+print("a * b =", a*b)
+print("a / b =", a/b)
+print("a // b =", a//b)
+print("a ** b =", a**b)
