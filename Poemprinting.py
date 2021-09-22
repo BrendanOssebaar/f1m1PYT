c3e1f1ca3661dcdd14e1434009b0612543b3d52f
BrendanOssebaar <34114@ma-web.nl>
2021-09-22 13:20:06 +0200

    Mijn Poem

---
+++ Poemprinting.py
@@ -0,0 +1,20 @@
+vers1 = "Mijn naam is Brendan Ossebaar"
+vers2 = "En ik ben 24 jaar"
+vers3 = "Ik woon bij mijn ouders in Purmerend"
+vers4 = "En een opleiding als Software Developer heb ik nooit gekend"
+vers5 = "Ik vind het leuk om co-op games te spelen"
+vers6 = "Maar ik ben niet weg van een paar kills in League te stelen"
+vers7 = "RTS games zijn mijn favouriet"
+vers8 = "Maar meer tijd in ander games gespeeld dan in Fallout 4 heb ik niet"
+vers9 = "Nu is ook codering een deel van mijn leven"
+vers10 = "Dus ga ik op school mijn alles geven"
+print(vers1)
+print(vers2)
+print(vers3)
+print(vers4)
+print(vers5)
+print(vers6)
+print(vers7)
+print(vers8)
+print(vers9)
+print(vers10)
