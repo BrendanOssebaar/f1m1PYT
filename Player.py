024194677d06d3ffaae944856cf4ebef79b900d9
BrendanOssebaar <34114@ma-web.nl>
2021-09-22 20:10:21 +0200

    PlayerStatistics

---
+++ Player.py
@@ -0,0 +1,23 @@
+
+print("Firstname + Lastname: ")
+a=input()
+b=input()
+print(a+ b)
+print("Main Weapon: ") 
+input()
+print("Armor Type: ")
+input()
+print("Main Skill: ")
+input()
+print("Movementspeed:")
+input()
+print("Languages:")
+input()
+Lives = int(input("Lives: "))
+    
+if Lives > 0:
+    print("You're still alive! Good Job")
+else: 
+    print("You are dead!")
+exit()
+
