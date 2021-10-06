0d4daa5058f5799d337b849611eb46473fce88cf
BrendanOssebaar <34114@ma-web.nl>
2021-10-06 12:38:53 +0200

    Family ties

---
+++ Family_ties.py
@@ -0,0 +1,11 @@
+n = "Brendan Ossebaar"
+l = 24
+w = "Purmerend"
+b = 2
+af = 22.5
+k = 2
+h = 1
+s = 2
+v = 5
+print("Hallo, mijn naam is " + n + " en ik ben "+ str(l) +" jaar oud. Ik woon in "+ w +" bij mijn ouders. Ik heb "+ str(b) +" oudere broers die niet meer thuis wonen. Wij hebben "+ str(k) +" katten, " + str(h) +" hond,")
+print(str(s) +" slangen en "+ str(v) +" vissen. Ik woon ongeveer"+ str(af) +" kilometer van school af")
